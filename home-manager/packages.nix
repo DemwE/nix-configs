67622fb434c06e733c5fc8676bbb7ac3d0035f29
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2025-06-27 17:24:28 +0200

    progress

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,23 +1,43 @@
 { pkgs, ... }:
-{ 
+{
   home.packages = [
-    pkgs.mpv
+    # Browsers
     pkgs.brave
-    pkgs.kitty
-    pkgs.vscode
-    pkgs.dunst
-    pkgs.waybar
-    pkgs.rofi-wayland
-    pkgs.xfce.thunar
-    pkgs.xfce.thunar-volman
-    pkgs.xfce.thunar-vcs-plugin
-    pkgs.xfce.thunar-archive-plugin
-    pkgs.catppuccin-cursors.mochaLavender
+
+    # Communication
     pkgs.discord
+
+    # Cursors/Themes
+    pkgs.catppuccin-cursors.mochaLavender
+
+    # Development
     pkgs.gh
+    pkgs.jetbrains.rust-rover
+    pkgs.jetbrains.webstorm
+    pkgs.nodejs_24
+    pkgs.rustc
+    pkgs.vscode
+
+    # File Manager
+    pkgs.xfce.thunar
+    pkgs.xfce.thunar-archive-plugin
+    pkgs.xfce.thunar-vcs-plugin
+    pkgs.xfce.thunar-volman
+
+    # Games
+    pkgs.prismlauncher
+
+    # Media
+    pkgs.mpv
+
+    # Utilities
+    pkgs.dunst
     pkgs.grim
+    pkgs.kitty
+    pkgs.pavucontrol
+    pkgs.rofi-wayland
     pkgs.slurp
     pkgs.swappy
-    pkgs.pavucontrol
+    pkgs.waybar
   ];
 }
