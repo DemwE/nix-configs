466e896b460f70297e480f211e4c9ff6959f07d7
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2025-09-25 15:44:34 +0200

    fix

--- modules/users/demwe/home.nix
+++ modules/users/demwe/home.nix
@@ -7,25 +7,14 @@ in {
   # Expose the full NixOS config to HM modules as `nixosConfig`
   home-manager.extraSpecialArgs = { nixosConfig = config; };
   home-manager.sharedModules = [
-    ({ lib, nixosConfig, ... }: {
-      options.my.paths.resources = lib.mkOption {
-        type = lib.types.path;
-        readOnly = true;
-        description = "Absolute path to repository resources directory (mirrors system-level option).";
-      };
-      options.my.paths.homeManager = lib.mkOption {
-        type = lib.types.path;
-        readOnly = true;
-        description = "Absolute path to repository home-manager configuration directory (mirrors system-level option).";
-      };
-      config.my.paths.resources = config.my.paths.resources;
-      config.my.paths.homeManager = config.my.paths.homeManager;
-      # Bridge system-level desktop options into Home Manager so HM modules see user overrides
-      # Use nixosConfig (passed via extraSpecialArgs) to get the NixOS-level value
-      config.my.desktop = nixosConfig.my.desktop;
-    })
+    # Ensure HM knows about the same custom options used by the system
+    (import ../../paths.nix)
     (import ../../theme/core.nix)
     (import ../../desktop.nix)
+    # Bridge system-level desktop values into Home Manager (after options are declared)
+    ({ nixosConfig, ... }: {
+      config.my.desktop = nixosConfig.my.desktop;
+    })
   ];
   home-manager.backupFileExtension = ".backup";
   home-manager.users.demwe = import hmRoot;
