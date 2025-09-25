39daa4eb75be3063e039d6e8a8b77f466309d9bf
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2025-09-25 15:35:02 +0200

    feat: restore default monitor and workspace configurations in desktop profile

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -71,8 +71,7 @@ in
     monitors = mkOption {
       type = types.listOf types.str;
       default = [
-        "DP-1,2560x1440@165,0x0,1"
-        "HDMI-A-1,1920x1080@60,-1080x-150,1,transform,1"
+        " , preferred, auto, 1"
       ];
       description = "List of Hyprland monitor definitions (as strings).";
       example = [
@@ -85,14 +84,7 @@ in
     workspaces = mkOption {
       type = types.listOf types.str;
       default = [
-        "1,monitor:DP-1"
-        "2,monitor:DP-1"
-        "3,monitor:DP-1"
-        "4,monitor:DP-1"
-        "5,monitor:DP-1"
-        "6,monitor:DP-1"
-        "7,monitor:DP-1"
-        "8,monitor:HDMI-A-1"
+        
       ];
       description = "List of Hyprland workspace assignments (strings like \"<n>,monitor:<name>\").";
       example = [
--- modules/users/demwe/home.nix
+++ modules/users/demwe/home.nix
@@ -4,8 +4,10 @@ let
 in {
   # Make Home Manager use the system's pkgs (with overlays applied)
   home-manager.useGlobalPkgs = true;
+  # Expose the full NixOS config to HM modules as `nixosConfig`
+  home-manager.extraSpecialArgs = { nixosConfig = config; };
   home-manager.sharedModules = [
-    ({ lib, ... }: {
+    ({ lib, nixosConfig, ... }: {
       options.my.paths.resources = lib.mkOption {
         type = lib.types.path;
         readOnly = true;
@@ -18,6 +20,9 @@ in {
       };
       config.my.paths.resources = config.my.paths.resources;
       config.my.paths.homeManager = config.my.paths.homeManager;
+      # Bridge system-level desktop options into Home Manager so HM modules see user overrides
+      # Use nixosConfig (passed via extraSpecialArgs) to get the NixOS-level value
+      config.my.desktop = nixosConfig.my.desktop;
     })
     (import ../../theme/core.nix)
     (import ../../desktop.nix)
