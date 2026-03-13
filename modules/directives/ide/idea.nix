26429b2a11b6aefceea63c2e0f10f1dc87ce803a
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-03-13 10:25:13 +0100

    feat: update IntelliJ IDEA package definition to include library path composition

--- modules/directives/ide/idea.nix
+++ modules/directives/ide/idea.nix
@@ -1,15 +1,33 @@
-# IntelliJ IDEA Ultimate package definition with Java toolchain in PATH
+# IntelliJ IDEA package definition with Java toolchain in PATH
 # pkgs: { idea }
 # Note: ~/.jdks/ symlinks (created by home/demwe/java.nix) let IDEA auto-detect all JDK versions.
 # ~/.toolchains/nodejs/bin injected so the GitHub Copilot plugin can find Node.js.
 
 pkgs:
+let
+  composeDeps = with pkgs; [
+    libGL
+    fontconfig
+    freetype
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+    stdenv.cc.cc.lib
+    zlib
+    mesa
+  ];
+
+  composeLibPath = pkgs.lib.makeLibraryPath composeDeps;
+in
 {
   idea = pkgs.unstable.jetbrains.idea.overrideAttrs (oldAttrs: {
-    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+
     postInstall = (oldAttrs.postInstall or "") + ''
       wrapProgram $out/bin/idea \
-        --run 'export PATH="$HOME/.toolchains/nodejs/bin:$PATH"'
+        --prefix PATH : "\$HOME/.toolchains/nodejs/bin" \
+        --prefix LD_LIBRARY_PATH : "${composeLibPath}"
     '';
   });
 }
