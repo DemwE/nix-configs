0cf4b959b4ccf421df3ad519de560a3d1596906e
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-03-16 13:34:49 +0100

    make switch require hostname argument (no default)

--- modules/directives/utils/switch.nix
+++ modules/directives/utils/switch.nix
@@ -1,12 +1,17 @@
 # Switch to a specific NixOS host
-# Usage: switch NixBook
+# Usage: switch <hostname> (e.g., switch NixBook, switch ServerName)
 
 pkgs: {
   switch = pkgs.writeShellApplication {
     name = "switch";
     runtimeInputs = [ pkgs.systemd ];
     text = ''
-      HOST="''${1:-NixBook}"
+      if [ -z "$1" ]; then
+        echo "Usage: switch <hostname>"
+        echo "Available hosts: NixBook"
+        exit 1
+      fi
+      HOST="$1"
       echo "Building host: $HOST"
       exec sudo nixos-rebuild switch --flake ".$HOST" --log-format bar-with-logs
     '';
