5ded9291c7e1619668a71eb742e28769fc9d0f0c
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-03-16 12:57:56 +0100

    move common modules to global default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -6,11 +6,19 @@
     ./groups.nix
     ./theme/core.nix
     ./overlays
+
+    # Common modules (available for all hosts)
+    ./common/boot/default.nix
+    ./common/networking/default.nix
+    ./common/services/default.nix
+    ./common/packages/default.nix
+
     # Aggregated collections
     ./users
     ./system
     ./features
+
     # Host-specific modules
-    ./hosts/NixBook/default.nix
+    ./hosts/NixBook
   ];
 }
--- modules/hosts/NixBook/default.nix
+++ modules/hosts/NixBook/default.nix
@@ -1,16 +1,8 @@
 {
-  pkgs-unstable ? null,
-  ...
-}:
-{
   imports = [
-    ./hardware-configuration.nix
-    ../../common/boot/default.nix
-    ../../common/networking/default.nix
-    ../../common/services/default.nix
-    ../../common/packages/default.nix
     ./boot.nix
     ./networking.nix
     ./features.nix
+    ./hardware-configuration.nix
   ];
 }
