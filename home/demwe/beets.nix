ffd8fe1cc95510a46303fb817d37330c446e0cc6
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-03-27 12:08:10 +0100

    refactor beets configuration: simplify package definition and clean up comments

--- home/demwe/beets.nix
+++ home/demwe/beets.nix
@@ -4,75 +4,72 @@
   programs.beets = {
     enable = true;
     
-    # Override the default package to ensure we have the necessary plugins enabled
-    package = pkgs.beets.override {
-      pluginOverrides = {
-        discogs = { enable = true; };
-        fetchart = { enable = true; };
-        scrub = { enable = true; };
-      };
-    };
+    # Simple package definition - most plugins are included by default
+    package = pkgs.beets;
 
     settings = {
-      directory = "~/Music";      # Where your processed music will live
-      library = "~/Music/library.db"; # Beets internal database location
+      directory = "~/Music";
+      library = "~/Music/library.db";
 
       import = {
-        move = true;               # Move files from source to destination
-        write = true;              # Write metadata tags to the files
-        autotag = true;            # Search for metadata automatically
-        resume = "ask";            # Ask to resume interrupted imports
+        move = true;
+        write = true;
+        autotag = true;
+        resume = "ask";
       };
 
-      # Essential plugins for your workflow
+      # List all plugins you want to use here
       plugins = [
-        "discogs"   # Better metadata for niche/electronic/modern releases
-        "fetchart"  # Downloads cover.jpg
-        "info"      # Allows checking tags with 'beet info'
-        "mbsync"    # Syncs extra info from MusicBrainz (like ISRC)
-        "scrub"     # Cleans existing tags before writing new ones
+        "discogs"
+        "fetchart"
+        "info"
+        "mbsync"
+        "scrub"
+        "embedart"
       ];
 
       # --- TAG MANAGEMENT ---
-
-      # The 'scrub' plugin removes all non-essential tags
       scrub = {
         auto = true;
       };
 
-      # Manually zero out tags you explicitly don't want
       item_fields = {
-        comment = "''";
-        copyright = "''";
-        publisher = "''";
+        comment = "";
+        copyright = "";
+        publisher = "";
+        asin = "";
+        barcode = "";
+        isrc = "$isrc";
       };
 
       # --- ARTWORK ---
-
       fetchart = {
         auto = true;
-        fname = "cover"; # Saves as 'cover.jpg' exactly as in your example
+        fname = "cover";
+        store_source = true;
+        maxwidth = 1200;
+        sources = [ "filesystem" "discogs" "itunes" "amazon" ];
       };
 
-      # --- DIRECTORY STRUCTURE (Your specific Pattern) ---
-      # Logic: Artist - Album (Year) / [Disc X /] Track - Title
+      embedart = {
+        auto = true;
+        ifempty = false; # Wymusza osadzenie nawet jeśli plik ma już jakąś okładkę
+        remove_art = false;
+      };
+
+      # --- DIRECTORY STRUCTURE ---
       paths = {
         default = "$albumartist - $album ($year)/%if{disctotal,Disc $disc/}$track - $title";
       };
 
       # --- DATA HANDLING ---
-
-      # Forces Beets to use only the Year part for the $year field
-      original_date = false; # Set to true if you prefer the original release year
+      original_date = false;
       
-      # UI settings for a better terminal experience
       ui = {
         color = true;
       };
 
-      # Required for MusicBrainz/Discogs API identification
       musicbrainz = {
-        # Using a generic user agent for the API
         useragent = "beets-nixos-user/1.0.0";
       };
     };
