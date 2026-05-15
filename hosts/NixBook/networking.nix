f4036ef25153a749d6dc657cf76ba97b6409731f
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-05-15 20:00:09 +0200

    update networking configurations: add comments for clarity on allowed TCP port ranges

--- hosts/NixBook/networking.nix
+++ hosts/NixBook/networking.nix
@@ -6,6 +6,7 @@
     firewall = {
       enable = true;
       allowedTCPPortRanges = [
+        # Development ports
         { from = 7000; to = 7100; }
         { from = 3000; to = 3100; }
       ];
