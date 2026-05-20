dd42ec700bb0104ffae04872e24ec843928ddce5
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-05-20 20:38:17 +0200

    update

--- hosts/NixBook/preservation.nix
+++ hosts/NixBook/preservation.nix
@@ -46,9 +46,9 @@
 
       echo "--> checking for old @root subvolumes to clean up..."
       cd /btrfs/old_roots
-      ls -d @root_* | sort | head -n -10 | while read -r old_root; do
+      ls -d @root_* | sort | head -n -8 | while read -r old_root; do
           echo "--> removing old root subvolume: /btrfs/old_roots/$old_root"
-          btrfs subvolume delete "/btrfs/old_roots/$old_root"
+          btrfs subvolume delete -R "/btrfs/old_roots/$old_root"
       done
       cd /
 
