9f9cfd695bfd3a75d1fcf8ae340f0cbea9996eaf
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2026-06-25 23:59:52 +0200

    feat: add nix-flatpak configuration and remove obsolete tamplates.nix

--- home/demwe/tamplates.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-
-{
-  # Expose repository templates in XDG Templates (~ /Templates by default).
-  home.file."Templates" = {
-    source = config.my.paths.resources + "/templates";
-    recursive = true;
-  };
-}
